1e501e66d2af8003a32d137fd247b6442ee37571
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-03-16 00:24:34 +0100

    Add link to article in ArticlePreview

--- src/article/article-preview/ArticlePreview.tsx
+++ src/article/article-preview/ArticlePreview.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { ArticlePreviewHeader } from "./article-preview-header";
 import "./ArticlePreview.css";
 import { ArticlePreviewProps } from "./types";
@@ -7,9 +8,13 @@ export const ArticlePreview = ({ article }: ArticlePreviewProps): JSX.Element =>
   return (
     <li className="article-preview">
       <ArticlePreviewHeader article={article} />
-      
-      <h2>{article.title}</h2>
-      <p>{article.description}</p>
+      <Link
+        to={`/${article.slug}`}
+        className="article-preview-link"
+      >
+        <h2>{article.title}</h2>
+        <p className="cde">{article.description}</p>
+      </Link>
     </li>
   )
 }
