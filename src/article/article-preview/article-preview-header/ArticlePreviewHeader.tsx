a15d699cc7a8636265a9296ec2546f601019b8ae
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2026-03-16 09:36:13 +0100

    add image loading for user with placeholder

--- src/article/article-preview/article-preview-header/ArticlePreviewHeader.tsx
+++ src/article/article-preview/article-preview-header/ArticlePreviewHeader.tsx
@@ -3,11 +3,49 @@ import { ArticlePreviewProps } from "../types";
 import "./ArticlePreviewHeader.css";
 import { FavoriteArticleButton } from "article/favorite-article-button";
 import { FiUser } from "react-icons/fi";
+import { useEffect, useState } from "react";
 
+
+const checkImage = (url: string): Promise<boolean> => {
+  return new Promise(resolve => {
+    const img = new Image();
+
+    img.onload = () => {
+      cleanup();
+      resolve(true);
+    }
+    img.onerror = () => {
+      cleanup();
+      resolve(false);
+    }
+
+    function cleanup() {
+      img.onload = null;
+      img.onerror = null;
+    }
+
+    img.src = url;
+  });
+}
 
 export const ArticlePreviewHeader = ({ article }: ArticlePreviewProps): JSX.Element => {
 
   const { author, createdAt } = article;
+
+  const [isImageLoading, setIsImageLoading] = useState(true);
+  const [isImageValid, setIsImageValid] = useState(false);
+
+  const imageURL = author.image;
+
+  useEffect(() => {
+    const tmp = async () => {
+      setIsImageValid(await checkImage(imageURL));
+      setIsImageLoading(false);
+    }
+
+    tmp();
+  }, [imageURL]);
+
 
   return (
     <header className="article-preview-header">
@@ -16,7 +54,14 @@ export const ArticlePreviewHeader = ({ article }: ArticlePreviewProps): JSX.Elem
           className="article-author-image"
           to={`/profile/${author.username}`}  
         >
-          <FiUser size={25}/>
+          {isImageLoading || !isImageValid
+            ? <FiUser size={25}/>
+            : <img
+              src={imageURL}
+              alt="User avatar"
+              style={{width: "100%", borderRadius: "inherit" }}
+            />
+          }  
         </Link>
         <div className="article-preview-details">
           <Link
